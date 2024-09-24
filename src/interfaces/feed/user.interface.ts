0d023bc33d4013a46c549522753f820ea7e8d85b
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2024-09-23 22:39:23 -0300

    refactor: UserInterface extends User from prisma and omits some arguments

--- src/interfaces/feed/user.interface.ts
+++ src/interfaces/feed/user.interface.ts
@@ -1,5 +1,8 @@
-export default interface User {
+import { User } from "@prisma/client"
+
+export default interface UserInterface extends Omit<User, "createdAt" | "updatedAt" | "password"> {
   id: string
   username: string
+  createdAt?: Date
   password?: string
 }

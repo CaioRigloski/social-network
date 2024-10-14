f5a525aadfe875919cb9287633b6afe6be20a308
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2024-10-14 15:37:51 -0300

    feat: friend suggestions moved from page to a component, mutate now filter the user id after a friend request sent

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,38 +1,21 @@
 'use client'
 
-import Post from "@/components/feed/Post"
 import { Button } from "@/components/ui/button"
 import { AlertDialog, AlertDialogHeader, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import Post from "@/components/feed/Post"
+import NewPost from "@/components/common/NewPost"
 
 import useSWR from "swr"
 
-import { newFriendSchema } from "@/lib/zod"
-import { sendFriendRequest, signOutAction } from "./actions"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
-import { friendsSuggestionsFetcher, postsFetcher } from "@/lib/swr"
-import NewPost from "@/components/common/NewPost"
+import { signOutAction } from "./actions"
+import { postsFetcher } from "@/lib/swr"
+import FriendSuggestions from "@/components/feed/FriendsSugestions"
 
 
 export default function Feed() {
   const postsData = useSWR("/api/feed/get-posts", postsFetcher)
-  const friendsSuggestions = useSWR("/api/user/get-friend-suggestions", friendsSuggestionsFetcher)
 
-  const addNewFriendForm = useForm<z.infer<typeof newFriendSchema>>({
-    resolver: zodResolver(newFriendSchema),
-  })
-
-  async function mutateFriendsSuggestions(newFriendId: string) {
-    addNewFriendForm.setValue("newFriendId", newFriendId)
-    await sendFriendRequest(addNewFriendForm.getValues())
-    friendsSuggestions.mutate(friendsSuggestions.data)
-  }
-  
   return (
     <main className="grid grid-rows-[auto_1fr] grid-cols-1 h-screen">
       <AlertDialog>
@@ -68,23 +51,8 @@ export default function Feed() {
             </AlertDescription>
           </Alert>
         }
-        
       </div>
-      <ScrollArea className="h-72 w-48 rounded-md border">
-        <div className="p-4">
-          <h4 className="mb-4 text-sm font-medium leading-none">Friend suggestions</h4>
-          {
-            friendsSuggestions.data?.map((suggestion) => 
-              <div key={"suggestion" + suggestion.id}>
-                <div className="text-sm">
-                  {suggestion.username}
-                </div>
-                <Button type="button" onClick={() => mutateFriendsSuggestions(suggestion.id)}>ADD</Button>
-                <Separator className="my-2" />
-              </div>
-            )}
-        </div>
-      </ScrollArea>
+      <FriendSuggestions/>
       <form action={signOutAction}>
         <Button type="submit">Sign Out</Button>
       </form>
---
+++ src/components/feed/FriendSuggestions.tsx
@@ -0,0 +1,43 @@
+import { sendFriendRequest } from "@/app/feed/actions"
+import { friendsSuggestionsFetcher } from "@/lib/swr"
+import { newFriendSchema } from "@/lib/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import useSWR from "swr"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { z } from "zod"
+
+
+export default function FriendSuggestions() {
+  const friendsSuggestions = useSWR("/api/user/get-friend-suggestions", friendsSuggestionsFetcher)
+
+  const addNewFriendForm = useForm<z.infer<typeof newFriendSchema>>({
+    resolver: zodResolver(newFriendSchema),
+  })
+
+  async function mutateFriendsSuggestions(newFriendId: string) {
+    addNewFriendForm.setValue("newFriendId", newFriendId)
+    await sendFriendRequest(addNewFriendForm.getValues())
+    friendsSuggestions.mutate(friendsSuggestions.data?.filter(user => user.id !== newFriendId))
+  }
+
+  return (
+  <ScrollArea className="h-72 w-48 rounded-md border">
+    <div className="p-4">
+      <h4 className="mb-4 text-sm font-medium leading-none">Friend suggestions</h4>
+      {
+        friendsSuggestions.data?.map((suggestion) => 
+          <div key={"suggestion" + suggestion.id}>
+            <div className="text-sm">
+              {suggestion.username}
+            </div>
+            <Button type="button" onClick={() => mutateFriendsSuggestions(suggestion.id)}>ADD</Button>
+            <Separator className="my-2" />
+          </div>
+        )}
+    </div>
+  </ScrollArea>
+  )
+}

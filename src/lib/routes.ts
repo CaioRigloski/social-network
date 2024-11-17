aa4eefa0635468fcf44e2bfc492dcafdb63c312f
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2024-11-17 15:58:32 -0300

    feat: Commented middleware, now it redirects to the derired path after authentication with search params 'from', instead always to /feed

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,5 @@
-export const PUBLIC_ROUTES = ["/user/sign-up"]
+export const SIGN_IN_URL = "/user/sign-in"
 
-export const AUTHENTICATED_REDIRECT = ["/user/sign-in", "/user/sign-up"]
+export const AUTHENTICATED_REDIRECT = [SIGN_IN_URL, "/user/sign-up"]
+
+export const PUBLIC_ROUTES = ["/user/sign-up", SIGN_IN_URL]

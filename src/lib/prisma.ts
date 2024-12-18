b83d6db861317b24ed4f13398a0dab1fc87958ad
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2024-12-18 14:48:21 -0300

    feat: create reusable objects for prisma queries

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,3 +5,72 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma || new PrismaClient()
  
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+// Query selects
+export const userSelect = {
+  id: true,
+  username: true,
+  profilePicture: true
+}
+
+export const commentSelect = {
+  id: true,
+  text: true,
+  user: userSelect
+}
+
+export const likeSelect = {
+  id: true,
+  user: userSelect
+}
+
+export const postSelect = {
+  ...userSelect,
+  posts: {
+    select: {
+      id: true,
+      picture: true,
+      comments: {
+        select: {
+          ...commentSelect
+        }
+      },
+      likes: {
+        select: {
+          ...likeSelect
+        }
+      }
+    }
+  }
+}
+
+export const messageSelect = {
+  id: true,
+  user: userSelect,
+  chatId: true,
+  text: true,
+  createdAt: true
+}
+
+export const frendSuggestionsSelect= {
+  friends: {
+    select: {
+      id: true
+    }
+  },
+  friendOf: {
+    select: {
+      id: true
+    }
+  },
+  friendRequestOf: {
+    select: {
+      id: true
+    }
+  },
+  friendRequests: {
+    select: {
+      id: true
+    }
+  }
+}

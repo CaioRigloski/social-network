7c99b331a73919a64ad8565e53a88a1c17e6bc29
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-20 16:33:48 -0300

    feat: add link to own profile or the user profile conditionally

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,12 +2,18 @@ import UserInterface from "@/interfaces/feed/user.interface";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { imageFormats, path } from "@/lib/utils";
 import { User } from "next-auth";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export function AvatarComponent(props: { user: UserInterface | User }) {
+  const session = useSession()
+
   return (
-    <Avatar className="static">
-      <AvatarImage src={`/images/${path.profile}/${props.user.profilePicture}.${imageFormats.profilePicture}`} alt={`@${props.user.username}`} />
-      <AvatarFallback>{props.user.username?.charAt(0).toUpperCase()}</AvatarFallback>
-    </Avatar>
+    <Link href={props.user.id === session.data?.user?.id ? "/user/profile" : `/user/profile/${props.user.id}`}>
+      <Avatar className="static">
+        <AvatarImage src={`/images/${path.profile}/${props.user.profilePicture}.${imageFormats.profilePicture}`} alt={`@${props.user.username}`} />
+        <AvatarFallback>{props.user.username?.charAt(0).toUpperCase()}</AvatarFallback>
+      </Avatar>
+    </Link>
   )
 }

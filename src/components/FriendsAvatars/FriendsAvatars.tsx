456d06cf805a61cb5de62666a54793509b5be9fe
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-20 15:42:01 -0300

    feat: new component FriendsAvatars to show current friends

---
+++ src/components/FriendsAvatars/FriendsAvatars.tsx
@@ -0,0 +1,23 @@
+import { friendsFetcher } from "@/lib/swr"
+import { ScrollArea } from "../ui/scroll-area"
+import useSWR from "swr"
+import { AvatarComponent } from "../Avatar/Avatar"
+
+export function FriendsAvatars() {
+  const friends = useSWR("/api/user/get-friends", friendsFetcher)
+
+  return (
+    <ScrollArea className="h-72 w-56 max-w-56 max-h-72 border shadow-md self-start mt-[4rem]">
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">My friends</h4>
+        <div className="flex gap-2 flex-wrap">
+          {
+            friends.data?.map(friend =>
+              <AvatarComponent user={friend}/>
+            )
+          }
+        </div>
+    </div>
+    </ScrollArea>
+  )
+}

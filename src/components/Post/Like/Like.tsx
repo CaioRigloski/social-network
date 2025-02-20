c2e77ff624b2c45a9ed058838bb14587f4a1f4bd
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-20 14:58:20 -0300

    refactor: like component uses new avatar component

--- src/components/Post/Like/Like.tsx
+++ src/components/Post/Like/Like.tsx
@@ -1,21 +1,16 @@
 
 import { Separator } from "@/components/ui/separator"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { path } from "@/lib/utils"
 import LikeInterface from "@/interfaces/feed/like.interface"
+import { AvatarComponent } from "@/components/Avatar/Avatar"
 
 export function Like(props: { like: LikeInterface, isOwn?: boolean }) {
   const id = props.like.user.id
-  const profilePicture = props.like.user.profilePicture
   const username = props.like.user.username
 
   return (
     <>
       <div className="flex flex-row gap-2 items-center">
-        <Avatar>
-          <AvatarImage src={`/images/${path.profile}/${profilePicture}.jpeg`} alt={`@${username}`} />
-          <AvatarFallback>{username.charAt(0)}</AvatarFallback>
-        </Avatar>
+        <AvatarComponent user={props.like.user}/>
         <p className="text-sm font-semibold leading-6 text-gray-900"><a href={props.isOwn ? "/user/profile" : `user/profile/${id}`}>{username}</a></p>
       </div>
       <Separator/>

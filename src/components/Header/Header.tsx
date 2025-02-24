3352c607b80d4e6f1f4b0e0af7a452f4c42cad19
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-24 13:56:04 -0300

    style: add background color in header on hover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -65,13 +65,13 @@ export default function Header() {
   }
 
   return (
-    <header className="flex flex-row-reverse place-content-around after:content-[''] after:flex-1 *:flex-1 items-end p-4 w-auto min-w-screen sticky top-0 z-50 shadow-md">
+    <header className="flex flex-row-reverse place-content-around after:content-[''] after:flex-1 *:flex-1 items-end p-4 w-auto min-w-screen sticky top-0 z-50 shadow-md hover:bg-white">
       <div className="relative flex gap-x-4 justify-center">
         <div className="text-sm leading-6">
           <div  className="flex items-center gap-x-2">
             {session.data?.user && <AvatarComponent user={session.data.user}/>}
             <Link href="/user/profile">
-              <p className="font-semibold text-gray-900 text-white">
+              <p className="font-semibold text-gray-900">
                   {session.status === "authenticated" && session.data.user?.username}
               </p>
             </Link>

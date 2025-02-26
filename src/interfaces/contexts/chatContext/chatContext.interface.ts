c85c2b1f2f5998038edfa128cbb80b62cd7e570d
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-26 16:43:19 -0300

    feat: interfaces for chat context and provider

---
+++ src/interfaces/contexts/chatContext/chatContext.interface.ts
@@ -0,0 +1,6 @@
+import ChatInterface from "@/interfaces/chat/chat.interface";
+
+export default interface ChatContextInterface {
+  chat: ChatInterface | undefined
+  addChat: (chat: ChatInterface) => void
+}

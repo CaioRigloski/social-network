a2808ba90b92151e12f156adbd9a1f239c67f021
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-26 16:42:33 -0300

    feat: add chat context

---
+++ src/contexts/ChatContext/ChatContext.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import ChatInterface from '@/interfaces/chat/chat.interface'
+import ChatContextInterface from '@/interfaces/contexts/chatContext/chatContext.interface'
+import ChatContextProvider from '@/interfaces/contexts/chatContext/chatContextProvider.interface'
+import React, { createContext, useState, useContext } from 'react'
+
+
+const ChatContext = createContext<ChatContextInterface | undefined>(undefined)
+
+export const ChatProvider: React.FC<ChatContextProvider> = ({ children }) => {
+  const [chat, setChat] = useState<ChatInterface | undefined>(undefined)
+
+  const addChat = (chat: ChatInterface) => {
+    setChat(chat)
+  }
+
+  return (
+    <ChatContext.Provider value={{ chat, addChat }}>
+      {children}
+    </ChatContext.Provider>
+  )
+}
+
+export function useChat() {
+  const context = useContext(ChatContext)
+
+  if (!context) {
+    throw new Error('useChat must be used within a ChatProvider')
+  }
+
+  return context
+}

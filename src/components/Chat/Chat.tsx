5737b3f718a5e691dfc281394ba2befa41fa7d98
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-26 16:45:51 -0300

    feat: chat component

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,103 @@
+
+import Link from "next/link"
+import { AvatarComponent } from "../Avatar/Avatar"
+import { Textarea } from "../ui/textarea"
+import { useSession } from "next-auth/react"
+import { useChat } from "@/contexts/ChatContext/ChatContext"
+import { useEffect, useState } from "react"
+import { detectEnterKey } from "@/lib/utils"
+import createOrUpdateChat from "@/app/user/chats/actions"
+import { SocketEvent } from "@/types/socket/event.type"
+import { socket } from "@/socket"
+import { ReceiveMessage } from "@/interfaces/socket/data/receiveMessage.interface"
+import ChatInterface from "@/interfaces/chat/chat.interface"
+import { mutate } from "swr"
+
+
+export function Chat() {
+  const session = useSession()
+  const [ inputValue, setInputValue ] = useState<string>("")
+  const { chat, addChat } = useChat()
+
+  useEffect(() => {
+    socket.on<SocketEvent>("receive_message", (msg: ReceiveMessage) => {
+      if(chat) {
+        addChat({ ...chat, messages: [...chat.messages, msg.message] })
+        mutate<ChatInterface[]>("/api/user/get-chats", data => {
+          data?.map(chat => {
+            if(chat.id === msg.message.chatId) {
+              chat.messages = [...chat.messages, msg.message]
+            }
+          })
+          return data
+        })
+      }
+    })
+    return () => {
+      socket.off("receive_message")
+    }
+  }, [chat])
+
+  async function sendMessage() {
+    let friendId: string | undefined = undefined
+
+    friendId = chat?.friend.id === session.data?.user?.id ? chat?.user.id : chat?.friend.id
+
+    if(inputValue.trim() && friendId) {
+      const newChat = await createOrUpdateChat({ text: inputValue, friendId: friendId, chatSchema: { roomId: chat?.id } })
+      socket.emit<SocketEvent>("send_message", { message: inputValue, roomId: newChat?.id})
+      setInputValue("")
+    }
+  }
+
+  return (
+    <div className="flex flex-1 flex-col gap-4 pb-2 w-[25rem] max-w-[25rem] fixed bottom-0 right-5 z-50 bg-white">  
+      {
+        chat &&
+          <div key={chat.id} className="shadow-md">
+            <header className="grid flex-1 text-left text-sm leading-tight truncate font-semibold bg-gray-500 p-3 text-white">
+              <div className="text-sm leading-6 text-black">
+              {
+                chat?.friend.id === session.data?.user?.id ?
+                <Link href="/user/profile" className="flex items-center gap-x-2">
+                  <AvatarComponent user={chat.user}/>
+                  <p className="font-semibold text-gray-900 text-white">
+                      {chat.user.username}
+                  </p>
+                </Link>
+                :
+                <Link href="/user/profile" className="flex items-center gap-x-2">
+                  <AvatarComponent user={chat.friend}/>
+                  <p className="font-semibold text-gray-900 text-white">
+                      {chat.friend.username}
+                  </p>
+                </Link>
+              }
+              </div>
+            </header>
+            <div className="flex flex-1 flex-col gap-2 p-4 border min-h-[25rem] max-h-[25rem] overflow-y-auto">
+              {
+                chat?.messages.map(message =>
+                  message.user.id === session.data?.user?.id ?
+                  <span key={message.id} className="w-fit self-end h-fit flex flex-col">
+                    <p className="text-white p-2 bg-green-500 rounded-xl w-fit">{message.text}</p>
+                    <time className="ml-auto text-[0.50rem]" dateTime={message.createdAt.toString()}>
+                      { new Date(message.createdAt).toLocaleTimeString() }
+                    </time>
+                  </span>
+                  :
+                  <span key={message.id} className="w-fit flex flex-col">
+                    <p key={message.id} className="text-white p-3 bg-cyan-500 rounded-xl w-fit">{message.text}</p>
+                    <time className="ml-auto text-[0.50rem] self-start ml-0" dateTime={message.createdAt.toString()}>
+                      { new Date(message.createdAt).toLocaleTimeString() }
+                    </time>
+                  </span>
+                )
+              }
+            </div>
+              <Textarea value={inputValue} onChange={e => setInputValue(e.target.value)} onKeyUp={e => detectEnterKey(e) && sendMessage()} className="resize-none focus:!ring-transparent border border-2 gray-100" placeholder="Type here..."/>
+          </div>
+      }
+    </div>
+  )
+}
---
+++ src/components/Chat/ChatList/ChatList.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { socket } from '@/socket'
+import createOrUpdateChat from '@/app/user/chats/actions'
+import { chatsFetcher, friendsFetcher } from "@/lib/swr"
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
+import useSWR from "swr"
+import UserInterface from "@/interfaces/feed/user.interface"
+import { SocketEvent } from "@/types/socket/event.type"
+import { ReceiveMessage } from "@/interfaces/socket/data/receiveMessage.interface"
+import React from 'react'
+import { ScrollArea } from '../../ui/scroll-area'
+import { useChat } from '@/contexts/ChatContext/ChatContext'
+
+
+export function ChatList() {
+  const session = useSession()
+
+  const chats = useSWR("/api/user/get-chats", chatsFetcher)
+  const {chat, addChat} = useChat()
+  const friends = useSWR("/api/user/get-friends", friendsFetcher)
+
+  const [ activeChatId, setActiveChatId ] = useState<string | undefined>()
+  const [ newFriendChat, setNewFriendChat ] = useState<UserInterface | null>()
+  const [ inputValue, setInputValue ] = useState<string>("")
+  
+  useEffect(() => {
+    socket.on<SocketEvent>("receive_message", (msg: ReceiveMessage) => {
+      chats.mutate(data => {
+        data?.map(chat => {
+          if(chat.id === msg.message.chatId) {
+            [...chat.messages, msg.message]
+          }
+        })
+        return data
+      })
+    })
+    return () => {
+      socket.off("receive_message")
+    }
+  }, [chats])
+
+  async function sendMessage() {
+    let friendId: string | undefined = undefined
+
+    if(newFriendChat) {
+      friendId = newFriendChat.id
+    } else {
+      chats.data?.map(chat => {
+        if(chat.id === activeChatId) {
+          friendId = chat.friend.id === session.data?.user?.id ? chat?.user.id : chat?.friend.id 
+        }
+      })
+    }
+
+    if(inputValue.trim() && friendId) {
+      const newChat = await createOrUpdateChat({ text: inputValue, friendId: friendId, chatSchema: { roomId: activeChatId } })
+      socket.emit<SocketEvent>("send_message", { message: inputValue, roomId: newChat?.id})
+      setInputValue("")
+    }
+  }
+
+  return (
+    <ScrollArea className="shadow-md">
+      {
+        chats.data?.map((chat) => (
+          <div key={chat.id} onClick={() => addChat(chat)} className="flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-[20rem] cursor-pointer">
+            <div className="flex w-full items-center gap-2">
+              <span>
+              {
+                chat?.friend.id === session.data?.user?.id ?
+                <a href={`/user/profile/${chat.user.id}`}>
+                  <span>{chat.user.username}</span>
+                </a>
+                :
+                <a href={`/user/profile/${chat.friend.id}`}>
+                  <span>{chat.friend.username}</span>
+                </a>
+              }
+              </span>
+              <span className="ml-auto text-xs">
+                <time className="ml-auto text-xs" dateTime={chat.messages.at(-1)?.createdAt.toString()}>
+                  {chat.messages.at(-1)?.createdAt.toLocaleString()}
+                </time>
+              </span>
+            </div>
+            <span className="line-clamp-2 w-[260px] whitespace-break-spaces text-xs">
+              {chat.messages.at(-1)?.text}
+            </span>
+          </div>
+        ))
+      }
+    </ScrollArea>
+    
+  )
+}

e0a04585594b0506d7df2e4945d97026bb4533dd
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-27 18:00:41 -0300

    feat: show only chats with at leat 1 message

--- src/components/Chat/ChatList/ChatList.tsx
+++ src/components/Chat/ChatList/ChatList.tsx
@@ -65,30 +65,31 @@ export function ChatList() {
     <ScrollArea className="h-[20rem]">
       {
         chats.data?.map((chat) => (
-          <div key={chat.id} onClick={() => addChat(chat)} className="flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-[20rem] cursor-pointer">
-            <div className="flex w-full items-center gap-2">
-              <span>
-              {
-                chat?.friend.id === session.data?.user?.id ?
-                <a href={`/user/profile/${chat.user.id}`}>
-                  <span>{chat.user.username}</span>
-                </a>
-                :
-                <a href={`/user/profile/${chat.friend.id}`}>
-                  <span>{chat.friend.username}</span>
-                </a>
-              }
-              </span>
-              <span className="ml-auto text-xs">
-                <time className="ml-auto text-xs" dateTime={chat.messages.at(-1)?.createdAt.toString()}>
-                  {chat.messages.at(-1)?.createdAt.toLocaleString()}
-                </time>
+          chat.messages.length > 0 &&
+            <div key={chat.id} onClick={() => addChat(chat)} className="flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-[20rem] cursor-pointer">
+              <div className="flex w-full items-center gap-2">
+                <span>
+                {
+                  chat?.friend.id === session.data?.user?.id ?
+                  <a href={`/user/profile/${chat.user.id}`}>
+                    <span>{chat.user.username}</span>
+                  </a>
+                  :
+                  <a href={`/user/profile/${chat.friend.id}`}>
+                    <span>{chat.friend.username}</span>
+                  </a>
+                }
+                </span>
+                <span className="ml-auto text-xs">
+                  <time className="ml-auto text-xs" dateTime={chat.messages.at(-1)?.createdAt.toString()}>
+                    {chat.messages.at(-1)?.createdAt.toLocaleString()}
+                  </time>
+                </span>
+              </div>
+              <span className="line-clamp-2 w-[260px] whitespace-break-spaces text-xs">
+                {chat.messages.at(-1)?.text}
               </span>
             </div>
-            <span className="line-clamp-2 w-[260px] whitespace-break-spaces text-xs">
-              {chat.messages.at(-1)?.text}
-            </span>
-          </div>
         ))
       }
     </ScrollArea>

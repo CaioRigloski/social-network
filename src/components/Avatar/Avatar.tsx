8f53140e9199738813e33e502399eb3f02b4df45
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-02-28 14:55:54 -0300

    fix: avatar component forward ref

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,19 +1,29 @@
-import UserInterface from "@/interfaces/feed/user.interface";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { imageFormats, path } from "@/lib/utils";
-import { User } from "next-auth";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
+import UserInterface from "@/interfaces/feed/user.interface"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import { imageFormats, path } from "@/lib/utils"
+import { User } from "next-auth"
+import Link from "next/link"
+import { useSession } from "next-auth/react"
+import { forwardRef } from "react"
 
-export function AvatarComponent(props: { user: UserInterface | User }) {
-  const session = useSession()
+export const AvatarComponent = forwardRef<HTMLDivElement, { user: UserInterface | User }>(
+  (props, ref) => {
+    const session = useSession()
 
-  return (
-    <Link href={props.user.id === session.data?.user?.id ? "/user/profile" : `/user/profile/${props.user.id}`} title={props.user.username}>
-      <Avatar className="static">
-        <AvatarImage src={`/images/${path.profile}/${props.user.profilePicture}.${imageFormats.profilePicture}`} alt={`@${props.user.username}`} />
-        <AvatarFallback>{props.user.username?.charAt(0).toUpperCase()}</AvatarFallback>
-      </Avatar>
-    </Link>
-  )
-}
+    return (
+      <Link href={props.user.id === session.data?.user?.id ? "/user/profile" : `/user/profile/${props.user.id}`} title={props.user.username}>
+        <Avatar ref={ref} className="static">
+          <AvatarImage 
+            src={`/images/${path.profile}/${props.user.profilePicture}.${imageFormats.profilePicture}`} 
+            alt={`@${props.user.username}`} 
+          />
+          <AvatarFallback>
+            {props.user.username?.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </Link>
+    )
+  }
+)
+
+AvatarComponent.displayName = 'AvatarComponent'

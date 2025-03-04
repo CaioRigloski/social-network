b08520e65043023c0e5c09cee51cddc67ba10226
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-03 23:16:07 -0300

    refactor: migrate server to TypeScript and update configurations

--- server.ts
+++ server.ts
@@ -1,30 +1,33 @@
 import { createServer } from "node:http"
 import next from "next"
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
+import { SocketEvent } from "./src/types/socket/event.type"
 
 const dev = process.env.NODE_ENV !== "production"
 const hostname = "localhost"
 const port = 3000
-// when using middleware `hostname` and `port` must be provided below
+
 const app = next({ dev, hostname, port })
 const handler = app.getRequestHandler()
 
 app.prepare().then(async () => {
   const httpServer = createServer(handler)
-
   const io = new Server(httpServer)
 
-  io.on("connection", (socket) => {
-    console.log("connected")
+  io.on("connection", (socket: Socket) => {
+    console.log("Client connected")
 
-    // chat functions
-    socket.on("send_message", data => {
+    socket.on("send_message", (data: unknown) => {
       io.emit('receive_message', data)
+    })
+
+    socket.on("disconnect", () => {
+      console.log("Client disconnected")
     })
   })
 
   httpServer
-    .once("error", (err) => {
+    .once("error", (err: Error) => {
       console.error(err)
       process.exit(1)
     })

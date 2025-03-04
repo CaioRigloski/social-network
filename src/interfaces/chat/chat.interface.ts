b8f91b8b314c01ba1791383e1f1c16149c115dee
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-03 23:42:20 -0300

    feat: add updatedAt attribute to chat interface

--- src/interfaces/chat/chat.interface.ts
+++ src/interfaces/chat/chat.interface.ts
@@ -1,10 +1,11 @@
-import { Chat } from "@prisma/client";
-import UserInterface from "../feed/user.interface";
-import MessageInterface from "./message.interface";
+import { Chat } from "@prisma/client"
+import UserInterface from "../feed/user.interface"
+import MessageInterface from "./message.interface"
 
 export default interface ChatInterface extends Omit<Chat | "CreatedAt", "UpdatedAt"> {
   id: string
   user: UserInterface
   friend: UserInterface
-  messages: MessageInterface[]
+  messages: MessageInterface[],
+  updatedAt: string
 }

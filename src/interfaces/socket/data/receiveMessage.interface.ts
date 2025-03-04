7f8b6141efeea5cd1f2568969d6adcd8a12bffe8
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-03 23:43:07 -0300

    remove: sender id from receive message interface

--- src/interfaces/socket/data/receiveMessage.interface.ts
+++ src/interfaces/socket/data/receiveMessage.interface.ts
@@ -1,8 +1,6 @@
 import MessageInterface from "@/interfaces/chat/message.interface"
-import UserInterface from "@/interfaces/feed/user.interface"
 
 export interface ReceiveMessage {
   message: MessageInterface
-  sender: UserInterface
   receiverId: string
 }

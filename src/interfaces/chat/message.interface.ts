abc192acedd685be105ea7c24cc519a67c993e65
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-08 01:04:20 -0300

    refactor: updatedAt overwritten as optional

--- src/interfaces/chat/message.interface.ts
+++ src/interfaces/chat/message.interface.ts
@@ -1,10 +1,11 @@
 import { Message } from "@prisma/client";
 import UserInterface from "../feed/user.interface";
 
-export default interface MessageInterface extends Omit<Message | "updatedAt", "UserId"> {
+export default interface MessageInterface extends Omit<Message, "userId" | "updatedAt"> {
   id: string
   user: UserInterface
   chatId: string
   text: string
   createdAt: Date
+  updatedAt?: Date
 }

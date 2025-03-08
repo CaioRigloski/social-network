66c2be92085f7553ace6b221cfd1bd0f64e468a4
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-08 01:02:44 -0300

    refactor: avoid updatedAt omit

--- src/interfaces/chat/chat.interface.ts
+++ src/interfaces/chat/chat.interface.ts
@@ -2,7 +2,7 @@ import { Chat } from "@prisma/client"
 import UserInterface from "../feed/user.interface"
 import MessageInterface from "./message.interface"
 
-export default interface ChatInterface extends Omit<Chat | "CreatedAt", "UpdatedAt"> {
+export default interface ChatInterface extends Omit<Chat, "createdAt"> {
   id: string
   user: UserInterface
   friend: UserInterface

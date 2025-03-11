1ddfa67b8891439e668e574ec9892e1e26fdb829
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-10 23:47:26 -0300

    feat: api routes paths

---
+++ src/lib/apiRoutes.ts
@@ -0,0 +1,17 @@
+export const API_ROUTES = {
+  user: {
+    chat: {
+      getChats: '/api/user/chat/get-chats',
+      getChat: '/api/user/chat/get-chat'
+    },
+    getFriendRequests: '/api/user/get-friend-requests',
+    getFriendSuggestions: '/api/user/get-friend-suggestions',
+    getFriends: '/api/user/get-friends',
+    getPosts: '/api/user/get-posts',
+    getUserInfo: '/api/user/get-user-info',
+  },
+  feed: {
+    getPosts: '/api/feed/get-posts',
+    postPublication: '/api/feed/post-publication'
+  }
+} as const

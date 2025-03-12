ff33ed9676288e72b96c9bb48167a0e7d57fcba7
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-11 23:32:59 -0300

    feat: uses the NewPostForm component logic

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,19 +1,16 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
-import { AlertDialog, AlertDialogHeader, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger, AlertDialogDescription } from "@/components/ui/alert-dialog"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Post } from "@/components/Post/Post"
-import { NewPostModal } from "@/components/Post/NewPostModal/NewPostModal"
 import { FriendSuggestions } from "@/components/feed/FriendSuggestions/FriendSuggestions"
 import { friendsFetcher, postsFetcher } from "@/lib/swr"
 import useSWR from "swr"
 import { FriendsAvatars } from "@/components/FriendsAvatars/FriendsAvatars"
 import { ChatAccordion } from "@/components/Chat/ChatAccordion/ChatAccordion"
-import { Textarea } from "@/components/ui/textarea"
-import { ImageIcon } from "@radix-ui/react-icons"
 import dynamic from "next/dynamic"
 import { useChat } from "@/contexts/ChatContext/ChatContext"
+import { NewPostForm } from "@/components/Post/NewPostForm/NewPostForm"
+import { useState } from "react"
 
 const Chat = dynamic(() => import('@/components/Chat/Chat').then(mod => mod.Chat), { ssr: false })
 
@@ -22,35 +19,31 @@ export default function Feed() {
   const friends = useSWR("/api/user/get-friends", friendsFetcher)
   const friendsIds = friends.data?.map(friend => friend.id)
   const postsData = useSWR(["/api/feed/get-posts", friendsIds], postsFetcher)
+  const [ hasImage, setHasImage ] = useState<boolean>(false)
+  const [isOnHover, setIsOnHover ] = useState<boolean>(false)
+
+  function handleImageSelected(hasImage: boolean) {
+    setHasImage(hasImage)
+  }
+
+  function handlePostFormHover(isOnHover: boolean) {
+    setIsOnHover(isOnHover)
+  }
+
+  function getImageClass() {
+    return `${hasImage && isOnHover ? 'opacity-50' : 'opacity-100'} transition-opacity duration-300 ease-in-out`;
+  }
 
   return (
     <main className="grid grid-cols-[1fr_max-content_1fr] place-items-center relative max-h-fit pt-[5rem]">
-      <div className="self-start sticky top-[10rem] flex flex-col gap-[5rem]">
+      <div className={`self-start sticky top-[10rem] flex flex-col gap-[5rem] ${getImageClass()}`}>
         <FriendsAvatars/>
         <FriendSuggestions/>
       </div>
       <div className="grid auto-rows-auto grid-cols-1 justify-items-center gap-4 w-[35rem] self-start">
-        <div className="flex flex-col w-full items-end">
-          <Textarea placeholder="What's on your mind?" className="resize-none"/>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button variant="ghost" className="w-fit place-self-end p-2" title="Add image">
-                <ImageIcon width={25} height={25}/>
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Add image</AlertDialogTitle>
-              </AlertDialogHeader>
-              <NewPostModal/>
-              <AlertDialogDescription>
-                Show a incredible picture!
-              </AlertDialogDescription>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
+        <NewPostForm onImageSelected={handleImageSelected} element={handlePostFormHover}/>
         {
-          postsData?.data?.map(post => <Post key={"post" + post?.id} post={post}/>)
+          postsData?.data?.map(post => <Post key={"post" + post?.id} post={post} className={getImageClass()}/>)
         }
         {
           postsData.data && postsData.data?.length > 0 && <p className="p-5 text-gray-300">No more posts.</p>
@@ -74,7 +67,7 @@ export default function Feed() {
           </Alert>
         }
       </div>
-      <div className="self-start sticky top-[10rem]">
+      <div className={`self-start sticky top-[10rem] ${getImageClass()}`}>
         <ChatAccordion/>
       </div>
       { chatId && <Chat/> }

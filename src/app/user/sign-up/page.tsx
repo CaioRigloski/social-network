14d6b36ca35753cea49c8d378f33d7179f23a6b8
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-13 00:28:00 -0300

    refactor: use new SignUpForm component on page

--- src/app/user/sign-up/page.tsx
+++ src/app/user/sign-up/page.tsx
@@ -1,67 +1,13 @@
 'use client'
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
- 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { createUser } from "./actions"
-import { signUpSchema } from "@/lib/zod"
 
+import { SignUpForm } from "@/components/SignUpForm/SignUpForm"
 
 export default function SignUp() {
-  const form = useForm<z.infer<typeof signUpSchema>>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      username: "",
-      password: ""
-    }
-  })
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(data => createUser(JSON.parse(JSON.stringify(data))))} className="space-y-8" method="POST">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="..." {...field} type="text"/>
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="..." {...field} type="password"/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignUpForm/>
+      </div>
+    </div>
   )
 }

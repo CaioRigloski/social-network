3c97b446e869d723de1d19d3fc53b7f37df3625c
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-13 15:19:28 -0300

    feat: login page redirects if user is authenticated

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import { LoginForm } from "@/components/LoginForm/LoginForm"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function SignIn() {
+  const session = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if(session.status === "authenticated") router.push("/feed")
+  }, [session])
+
+  if(session.status === "authenticated") return null
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">

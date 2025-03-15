87b09ec96b793c4ca7eb46d16c028dbb83d6fd68
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-15 15:54:33 -0300

    refactor: replace useSWR key with API_ROUTES path in FriendAvatars component

--- src/components/FriendsAvatars/FriendsAvatars.tsx
+++ src/components/FriendsAvatars/FriendsAvatars.tsx
@@ -2,9 +2,10 @@ import { friendsFetcher } from "@/lib/swr"
 import { ScrollArea } from "../ui/scroll-area"
 import useSWR from "swr"
 import { AvatarComponent } from "../Avatar/Avatar"
+import { API_ROUTES } from "@/lib/apiRoutes"
 
 export function FriendsAvatars() {
-  const friends = useSWR("/api/user/get-friends", friendsFetcher)
+  const friends = useSWR(API_ROUTES.user.getFriends, friendsFetcher)
 
   return (
     <ScrollArea className="h-72 w-56 max-w-56 max-h-72 border shadow-md self-start rounded-md">

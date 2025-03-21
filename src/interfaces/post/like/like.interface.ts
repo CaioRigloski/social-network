1709db82feaa20d5fc6b067b7c9772f90b65f1d5
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-21 13:49:27 -0300

    fix: import paths of UserInterface

--- src/interfaces/post/like/like.interface.ts
+++ src/interfaces/post/like/like.interface.ts
@@ -1,5 +1,5 @@
-import { Like } from "@prisma/client";
-import UserInterface from "./user.interface";
+import { Like } from "@prisma/client"
+import UserInterface from "@/interfaces/feed/user.interface"
 
 export default interface LikeInterface extends Omit<Like, "createdAt" | "updatedAt" | "userId" | "postId"> {
   id: string

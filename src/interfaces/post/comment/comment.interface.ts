4fa61efe3fd1b03ea4a3995385babbfbe2ceecb6
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-21 13:49:27 -0300

    fix: import paths of UserInterface

--- src/interfaces/post/comment/comment.interface.ts
+++ src/interfaces/post/comment/comment.interface.ts
@@ -1,5 +1,5 @@
 import { Comment } from "@prisma/client"
-import UserInterface from "./user.interface"
+import UserInterface from "@/interfaces/feed/user.interface"
 
 export default interface CommentInterface extends Omit<Comment, "createdAt" | "updatedAt" | "userId" | "postId"> {
   id: string

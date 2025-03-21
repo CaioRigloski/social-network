bcda36e0038f8d047134d0a260f18441a4cde661
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-21 16:54:11 -0300

    feat: add edit_message prop to SocketEvent type

--- src/types/socket/event.type.ts
+++ src/types/socket/event.type.ts
@@ -1,1 +1,1 @@
-export type SocketEvent = "send_message" | "receive_message" | "delete_message" | "join_room"
+export type SocketEvent = "send_message" | "receive_message" | "edit_message" | "delete_message" | "join_room"

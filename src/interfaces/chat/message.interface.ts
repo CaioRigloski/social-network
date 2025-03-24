b6753894476fd1755a98e86239167f31059c289d
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-24 14:59:02 -0300

    feat: add deleted boolean prop to message interface and in message select

--- src/interfaces/chat/message.interface.ts
+++ src/interfaces/chat/message.interface.ts
@@ -1,11 +1,12 @@
 import { Message } from "@prisma/client";
 import UserInterface from "../feed/user.interface";
 
-export default interface MessageInterface extends Omit<Message, "userId" | "updatedAt"> {
+export default interface MessageInterface extends Omit<Message, "userId" | "updatedAt" | "deleted"> {
   id: string
   user: UserInterface
   chatId: string
   text: string
   createdAt: Date
   updatedAt?: Date
+  deleted?: boolean
 }

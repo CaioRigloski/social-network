44cdacaa7769e7788d9a83af880c93f12b25454b
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-24 17:17:53 -0300

    refactor: use Dialog instead AlertDialog in Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -10,7 +10,7 @@ import { useSession } from "next-auth/react"
 import { mutate } from "swr"
 import { deletePost } from "@/app/post/actions"
 import { ChatBubbleIcon, HeartFilledIcon, HeartIcon } from "@radix-ui/react-icons"
-import { AlertDialog, AlertDialogHeader, AlertDialogContent, AlertDialogTitle, AlertDialogTrigger, AlertDialogDescription } from "@/components/ui/alert-dialog"
+import { Dialog, DialogHeader, DialogContent, DialogTitle, DialogTrigger, DialogDescription } from "@/components/ui/dialog"
 import { Like } from "./Like/Like"
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { MoreVertical } from "lucide-react"
@@ -93,7 +93,7 @@ export function Post(props: { post: PostInterface, className?: string }) {
   }
  
   return (
-    <Card className={`${props.className} shadow-md`}>
+    <Card className={`${props.className} shadow-md break-all`}>
       <CardHeader className="flex flex-row p-4">
         <div className="w-full">
           <CardTitle className="flex items-center gap-2 text-zinc-600 dark:text-sky-400/75">
@@ -123,8 +123,8 @@ export function Post(props: { post: PostInterface, className?: string }) {
       </CardHeader>
       {
         props.post.description &&
-        <CardDescription className="p-5">
-          <p className="break-words">{ props.post.description }</p>
+        <CardDescription className="p-5 overflow-y-auto comment-line-limit">
+          { props.post.description }
         </CardDescription>
       }
       <CardContent className="p-1 w-[35rem] h-[35rem] ml-auto mr-auto border">
@@ -132,24 +132,26 @@ export function Post(props: { post: PostInterface, className?: string }) {
       </CardContent>
       <CardFooter className="p-1 pb-4 flex flex-row gap-2 justify-end w-[32rem] ml-auto mr-auto">
         <div className="flex flex-col items-center gap-1">
-          <AlertDialog open={commentModalIsOpen} onOpenChange={() => setCommentModalIsOpen(!commentModalIsOpen)}>
-            <AlertDialogTrigger asChild>
+          <Dialog open={commentModalIsOpen} onOpenChange={() => setCommentModalIsOpen(!commentModalIsOpen)}>
+            <DialogTrigger asChild>
               <ChatBubbleIcon width={25} height={25} cursor={"pointer"}/>
-            </AlertDialogTrigger>
+            </DialogTrigger>
             <p className="cursor-default">{props.post.commentsCount}</p>
-
-            <AlertDialogContent className="max-w-[80vw] max-h-[95vh] w-[80vw] h-[95vh] grid grid-rows-[auto_1fr]">
-              <AlertDialogHeader className="flex flex-row gap-2">
+            <DialogContent className="max-w-[80vw] max-w-[80vw] max-h-[95vh] w-[80vw] h-[95vh] grid grid-rows-[auto_1fr] break-all">
+              <DialogHeader className="flex flex-row gap-2">
                 <AvatarComponent user={props.post.user}/>
                 <h3>
                   <Link href={`/user/profile/${props.post.user.id}`}>{props.post.user?.username}</Link>
                 </h3>
-              </AlertDialogHeader>
-              <AlertDialogTitle className="hidden">
+              </DialogHeader>
+              <DialogTitle className="hidden">
                 Post details.
-              </AlertDialogTitle>
+              </DialogTitle>
+              <DialogDescription className="overflow-y-auto">
+                { props.post.description }
+              </DialogDescription>
               <div className="grid grid-cols-3 grid-rows-1 gap-2">
-                <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2"/>
+                <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2 border"/>
                 {
                   props.post.comments && props.post.comments.length > 0 ?
                   <div className="col-span-1 overflow-y-scroll max-h-[70vh]">
@@ -164,11 +166,11 @@ export function Post(props: { post: PostInterface, className?: string }) {
                 }
                 <Textarea className="col-span-3 resize-none" placeholder="Leave a comment!" onChange={e => setComment(e.target.value)} onKeyUp={e => detectEnterKey(e) && commentAndMutatePostsData()}/>
               </div>
-              <AlertDialogDescription className="hidden">
+              <DialogDescription className="hidden">
                 See the posts details!
-              </AlertDialogDescription>
-            </AlertDialogContent>
-          </AlertDialog>
+              </DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="flex flex-col items-center gap-1">
           {
@@ -177,25 +179,25 @@ export function Post(props: { post: PostInterface, className?: string }) {
               :
               <HeartIcon width={25} height={25} cursor={"pointer"} onClick={likeAndMutatePostsData}/>
           }
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
+          <Dialog>
+            <DialogTrigger asChild>
               <p className="cursor-pointer">{props.post.likesCount}</p>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Likes</AlertDialogTitle>
-              </AlertDialogHeader>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Likes</DialogTitle>
+              </DialogHeader>
                 {
                   props.post.likesCount > 0 ?
                   props.post?.likes?.map(like => <Like key={like.id} like={like} isOwn={like.user.id === session.data?.user?.id}/>)
                   :
                   <p>No likes yet.</p>
                 }
-                <AlertDialogDescription>
+                <DialogDescription>
                   See who liked it!
-                </AlertDialogDescription>
-            </AlertDialogContent>
-          </AlertDialog>
+                </DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
       </CardFooter>
     </Card>

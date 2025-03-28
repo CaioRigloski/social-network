ab987000d8355762d4941c986b2fee7900f94e47
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-03-28 19:03:38 -0300

    refactor: remove unecessary div and apply it's style to other one

--- src/components/Post/CommentModal/CommentModal.tsx
+++ src/components/Post/CommentModal/CommentModal.tsx
@@ -7,10 +7,6 @@ import Link from "next/link"
 import { Comment } from "../Comment/Comment"
 import { detectEnterKey, imageFormats, path } from "@/lib/utils"
 import { useSession } from "next-auth/react"
-import { createNewComment } from "@/app/post/comment/actions"
-import { mutate } from "swr"
-import PostInterface from "@/interfaces/post/post.interface"
-import { API_ROUTES } from "@/lib/apiRoutes"
 import { useState } from "react"
 
 export function CommentModal(props: CommentModalInterface) {
@@ -18,46 +14,46 @@ export function CommentModal(props: CommentModalInterface) {
   const [ commentModalIsOpen, setCommentModalIsOpen ] = useState<boolean>(false)
 
   return (
-    <div className="flex flex-col items-center gap-1">
-      <Dialog open={commentModalIsOpen} onOpenChange={() => setCommentModalIsOpen(!commentModalIsOpen)}>
-        <DialogTrigger asChild>
-          <ChatBubbleIcon width={25} height={25} cursor={"pointer"}/>
-        </DialogTrigger>
-        <p className="cursor-default">{props.post.commentsCount}</p>
-        <DialogContent className="max-w-[80vw] max-w-[80vw] max-h-[95vh] w-[80vw] h-[95vh] grid grid-rows-[auto_auto_1fr] break-all">
-          <DialogHeader className="flex flex-row gap-2">
-            <AvatarComponent user={props.post.user}/>
-            <h3>
-              <Link href={`/user/profile/${props.post.user.id}`}>{props.post.user?.username}</Link>
-            </h3>
-          </DialogHeader>
-          <DialogTitle className="hidden">
-            Post details.
-          </DialogTitle>
-          <DialogDescription className="overflow-y-auto">
-            { props.post.description }
-          </DialogDescription>
-          <div className="grid grid-cols-3 grid-rows-1 gap-2">
-            <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2 border"/>
-            {
-              props.post.comments && props.post.comments.length > 0 ?
-              <div className="col-span-1 overflow-y-scroll max-h-[70vh]">
-                {
-                  props.post.comments?.map(comment => <Comment key={comment.id} postId={props.post.id} comment={comment} isOwn={comment.user.id === session.data?.user?.id}/>)
-                }
-              </div>
-              :
-              <div className="grid items-start text-center text-gray-400">
-                <p>No comments yet</p>
-              </div>
-            }
-            <Textarea className="col-span-3 resize-none" placeholder="Leave a comment!" onChange={e => props.commentOnChange(e)} onKeyUp={e => detectEnterKey(e) && props.commentOnKeyUp()} maxLength={500}/>
-          </div>
-          <DialogDescription className="hidden">
-            See the posts details!
-          </DialogDescription>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <Dialog open={commentModalIsOpen} onOpenChange={() => setCommentModalIsOpen(!commentModalIsOpen)}>
+      <DialogTrigger asChild>
+        <div className="grid justify-items-center cursor-pointer gap-1">
+          <ChatBubbleIcon width={25} height={25}/>
+          <p>{props.post.commentsCount}</p>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-w-[80vw] max-w-[80vw] max-h-[95vh] w-[80vw] h-[95vh] grid grid-rows-[auto_auto_1fr] break-all">
+        <DialogHeader className="flex flex-row gap-2">
+          <AvatarComponent user={props.post.user}/>
+          <h3>
+            <Link href={`/user/profile/${props.post.user.id}`}>{props.post.user?.username}</Link>
+          </h3>
+        </DialogHeader>
+        <DialogTitle className="hidden">
+          Post details.
+        </DialogTitle>
+        <DialogDescription className="overflow-y-auto">
+          { props.post.description }
+        </DialogDescription>
+        <div className="grid grid-cols-3 grid-rows-1 gap-2">
+          <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2 border"/>
+          {
+            props.post.comments && props.post.comments.length > 0 ?
+            <div className="col-span-1 overflow-y-scroll max-h-[70vh]">
+              {
+                props.post.comments?.map(comment => <Comment key={comment.id} postId={props.post.id} comment={comment} isOwn={comment.user.id === session.data?.user?.id}/>)
+              }
+            </div>
+            :
+            <div className="grid items-start text-center text-gray-400">
+              <p>No comments yet</p>
+            </div>
+          }
+          <Textarea className="col-span-3 resize-none" placeholder="Leave a comment!" onChange={e => props.commentOnChange(e)} onKeyUp={e => detectEnterKey(e) && props.commentOnKeyUp()} maxLength={500}/>
+        </div>
+        <DialogDescription className="hidden">
+          See the posts details!
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   )
 }

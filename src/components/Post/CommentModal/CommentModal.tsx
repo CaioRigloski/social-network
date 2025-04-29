059c5a1a331aa61e56b8da8d84a7fd42ea464179
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2025-04-29 17:31:17 -0300

    feat: styles for CommentModal component  with no picture

--- src/components/Post/CommentModal/CommentModal.tsx
+++ src/components/Post/CommentModal/CommentModal.tsx
@@ -12,6 +12,7 @@ import { createNewComment } from "@/app/post/comment/actions"
 import { mutate } from "swr"
 import PostInterface from "@/interfaces/post/post.interface"
 import { API_ROUTES } from "@/lib/apiRoutes"
+import { Separator } from "@/components/ui/separator"
 
 export function CommentModal(props: CommentModalInterface) {
   const session = useSession()
@@ -42,7 +43,7 @@ export function CommentModal(props: CommentModalInterface) {
           <p>{props.post.commentsCount}</p>
         </div>
       </DialogTrigger>
-      <DialogContent className="max-w-[80vw] max-w-[80vw] max-h-[95vh] w-[80vw] h-[95vh] grid grid-rows-[auto_auto_1fr] break-all">
+      <DialogContent className={`max-w-[80vw] max-w-[80vw] max-h-[95vh] ${props.post.picture ? "w-[80vw]" : "w-[40vw]"} h-[95vh] grid grid-rows-[auto_auto_auto_1fr] break-all`}>
         <DialogHeader className="flex flex-row gap-2">
           <AvatarComponent user={props.post.user}/>
           <h3>
@@ -55,8 +56,9 @@ export function CommentModal(props: CommentModalInterface) {
         <DialogDescription className="overflow-y-auto">
           { props.post.description }
         </DialogDescription>
-        <div className="grid grid-cols-3 grid-rows-1 gap-2">
-          <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2 border"/>
+        <Separator className="col-span-none"/>
+        <div className={`grid ${props.post.picture ? "grid-cols-3" : "grid-cols-1"}  grid-rows-[auto_max-content] gap-2`}>
+          { props.post.picture && <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full max-w-[60rem] max-h-[40rem] object-contain col-span-2 border"/> }
           {
             props.post.comments && props.post.comments.length > 0 ?
             <div className="col-span-1 overflow-y-scroll max-h-[70vh]">
@@ -69,7 +71,7 @@ export function CommentModal(props: CommentModalInterface) {
               <p>No comments yet</p>
             </div>
           }
-          <Textarea className="col-span-3 resize-none" placeholder="Leave a comment!" onChange={e => setComment(e.target.value)} onKeyDown={e => detectEnterKey(e) && commentAndMutatePostsData()} maxLength={500}/>
+          <Textarea className="col-span-3 resize-none h-[5rem] self-end" placeholder="Leave a comment!" onChange={e => setComment(e.target.value)} onKeyDown={e => detectEnterKey(e) && commentAndMutatePostsData()} maxLength={500}/>
         </div>
         <DialogDescription className="hidden">
           See the posts details!

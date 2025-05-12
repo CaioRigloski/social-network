8dbb894e2d5dbb3df2712861e8c2577e75eaa8a1
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-05-12 01:38:07 -0300

    feat: add generic unlike mutation to use it on any page

--- src/hooks/post.ts
+++ src/hooks/post.ts
@@ -1,4 +1,4 @@
-import { createNewLike } from "@/app/post/like/actions"
+import { createNewLike, unlike } from "@/app/post/like/actions"
 import LikeInterface from "@/interfaces/post/like/like.interface"
 import PostInterface from "@/interfaces/post/post.interface"
 import SearchResultInterface from "@/interfaces/search/searchResult/searchResult.interface"
@@ -19,9 +19,6 @@ export async function useLikeMutation(postId: string, key: Key) {
     })
   }
   
-  if(Array.isArray(key)) console.log(key[0])
-
-
   if(key === API_ROUTES.feed.getPosts) {
     createNewLike({postId: postId}).then((newLike) => {
       mutate<PostInterface[]>(key, data => mutatePost(data, newLike), false)
@@ -35,6 +32,41 @@ export async function useLikeMutation(postId: string, key: Key) {
           return {
             ...data,
             posts: mutatePost(data.posts, newLike)
+          };
+        }
+        return data;
+      }, false)
+    })
+  }
+}
+
+export async function useUnlikeMutation(postId: string, likeId: string, key: Key) {
+  function mutatePost(data: PostInterface[] | undefined, likeId: string) {
+    return data?.map(post => {
+      if (post.id === postId) {
+        return {
+          ...post,
+          likes: post.likes.filter(like => like.id !== likeId),
+          likesCount: post.likesCount - 1
+        }
+      }
+      return post
+    })
+  }
+  
+  if(key === API_ROUTES.feed.getPosts) {
+    unlike({postId: postId, likeId: likeId}).then(() => 
+      mutate<PostInterface[]>(key, data => mutatePost(data, likeId), false)
+    )
+  }
+
+  if(Array.isArray(key) && key[0] === API_ROUTES.search.getSearch) {
+    unlike({postId: postId, likeId: likeId}).then((newLike) => {
+      mutate<SearchResultInterface>(key, data => {
+        if (data) {
+          return {
+            ...data,
+            posts: mutatePost(data.posts, likeId)
           };
         }
         return data;

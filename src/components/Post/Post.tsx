baa9a9cdf668ba8288d67491fe35e518c0a248c8
Caio Rigloski <caio.henrique.rigloski@hotmail.com>
2025-05-11 23:39:10 -0300

    feat: add css variables for post width and post image width and applies it in components

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -43,7 +43,7 @@ export function Post(props: { post: PostInterface, className?: string }) {
   }
  
   return (
-    <Card className={`${props.className} shadow-md break-all whitespace-pre-wrap text-color`}>
+    <Card className={`${props.className} shadow-md break-all whitespace-pre-wrap text-color w-[var(--post-width)]`}>
       <CardHeader className="flex flex-row rounded-t-md bg-foreground h-14 items-center">
         <CardTitle className="flex items-center gap-2">
           <AvatarComponent user={props.post.user}/>
@@ -79,7 +79,7 @@ export function Post(props: { post: PostInterface, className?: string }) {
       { isExpanded && <button className="mt-1 text-xs leading-5 text-sky-700 ml-5" onClick={() => setIsExpanded(false)}>view less</button> }
       {
         props.post.picture &&
-        <CardContent className="p-1 w-[35rem] h-[35rem] ml-auto mr-auto border">
+        <CardContent className="p-1 w-[var(--post-width)] h-[var(--post-image-height)] ml-auto mr-auto border">
           <img alt="post picture" width={0} height={0} src={`/images/${path.posts}/${props.post.picture}.${imageFormats.posts}`} className="w-full h-full object-cover cursor-pointer" onClick={() => setCommentModalIsOpen(true)}/>
         </CardContent>
       }
